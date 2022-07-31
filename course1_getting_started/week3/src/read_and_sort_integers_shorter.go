89f9b5b0be29fb2fdc1eca19d434d385706e1665
Robotawi <mohamed.s.raessa@gmail.com>
2022-07-31 13:42:53 +0900

    Read ans sort integers improved shorter code

---
+++ course1_getting_started/week3/src/read_and_sort_integers_shorter.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	//init slice of integers
+	//loop to: ask user for inputs.
+	//if X, then quit the program, else append in the int slice and sort it
+
+	var input string
+	int_sli := []int{}
+
+	for {
+		fmt.Print("Please enter an integer: ")
+		fmt.Scan(&input)
+
+		if input == "X" {
+			break
+		} else {
+			int_val, _ := strconv.Atoi(input)
+			int_sli = append(int_sli, int_val)
+			sort.Slice(int_sli, func(i, j int) bool {
+				return int_sli[i] < int_sli[j]
+			})
+
+			fmt.Println(int_sli) //we can print a slice in go like Python 
+		}
+	}
+}
