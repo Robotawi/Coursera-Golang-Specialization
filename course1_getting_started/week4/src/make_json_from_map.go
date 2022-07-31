1e7f658bf29ecae7bb3ed419fe7f3a5323b9628c
Robotawi <mohamed.s.raessa@gmail.com>
2022-07-31 13:44:17 +0900

    Make JSON object from map

---
+++ course1_getting_started/week4/src/make_json_from_map.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+//NOTE:  json package only stringfiy fields start with capital letter
+// type AddressBook struct{
+// 	Name string `json:"Name"`
+// 	Address string `json"Address"`
+// }
+
+func main(){
+	var name string
+	var address string
+
+	fmt.Scan(&name, &address)
+
+	p:=map[string]string{
+		"Name": name,
+		"Address": address,
+	}
+	
+	json_obj, _ := json.Marshal(p)
+
+	fmt.Println(string(json_obj))
+}
